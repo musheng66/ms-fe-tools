a6546bd9e156b4014cd9ab4e88bca673d453e5fe
chenran <18611652288@163.com>
2020-01-02 15:58:04 +0800

    优化判断null的方法，将null，undefined，{}视为空

--- src/validate/index.js
+++ src/validate/index.js
@@ -5,7 +5,7 @@ class Validate {
    * @return {boolean} result
    */
   isObject (obj) {
-    return (obj && typeof obj === 'object' && !Array.isArray(obj))
+    return (!this.isNull(obj) && typeof obj === 'object' && !Array.isArray(obj))
   }
   /**
    * 判断是否为 null 或 undefined
@@ -13,20 +13,20 @@ class Validate {
    * @returns {boolean} result
    */
   isNull (val) {
-    if (typeof val === 'boolean') {
-      return false
+    if (typeof val === 'boolean') return false
+    if (typeof val === 'number') return false
+    if (typeof val === 'string') return false
+    if (typeof val === 'function') return false
+    if (typeof val === 'undefined') return true
+    if (typeof val === 'object') {
+      if (val instanceof Array) return false
+      if (val === null) return true
+      try {
+        if (JSON.stringify(val) === '{}') return true
+      } catch (e) {
+        return false
+      }
     }
-    if (typeof val === 'number') {
-      return false
-    }
-    if (val instanceof Array) {
-      if (val.length === 0) return true
-    } else if (val instanceof Object) {
-      if (JSON.stringify(val) === '{}') return true
-    } else {
-      return (String(val) === 'null' || String(val) === 'undefined')
-    }
-    return false
   }
   /**
    * URL地址是否合法
@@ -75,25 +75,6 @@ class Validate {
    */
   isIdCard (code) {
     if (this.isNull(code)) return false
-    // let result = false
-    // const city = {
-    //   11: '北京', 12: '天津', 13: '河北', 14: '山西', 15: '内蒙古',
-    //   21: '辽宁', 22: '吉林', 23: '黑龙江 ',
-    //   31: '上海', 32: '江苏', 33: '浙江', 34: '安徽', 35: '福建', 36: '江西', 37: '山东',
-    //   41: '河南', 42: '湖北 ', 43: '湖南', 44: '广东', 45: '广西', 46: '海南',
-    //   50: '重庆', 51: '四川', 52: '贵州', 53: '云南', 54: '西藏 ',
-    //   61: '陕西', 62: '甘肃', 63: '青海', 64: '宁夏', 65: '新疆',
-    //   71: '台湾',
-    //   81: '香港', 82: '澳门',
-    //   91: '国外 '
-    // }
-    // if (code.length === 18) {
-    //   if (!code || !/(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(code)) {
-    //   } else if (!city[code.substr(0, 2)]) {
-    //   } else {
-    //     result = true
-    //   }
-    // }
     const isIdCard = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/
     return isIdCard.test(code)
   }
