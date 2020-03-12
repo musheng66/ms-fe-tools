587dd88f611f938a2bd46314e00cda2932504811
musheng <425893642@qq.com>
2020-03-12 14:46:53 +0800

    优化common类使用方法

--- test/common/random.test.js
+++ test/common/random.test.js
@@ -1,7 +1,5 @@
 import { common } from '../../src/index'
 
-const random = common.random
-
 test('生成随机字符串', () => {
-  expect(random.guid()).not.toBe(null)
+  expect(common.random.guid()).not.toBe(null)
 })
